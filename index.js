703644b443cd13a438670cd277887a5270475769
James Bubb <jpbubb82@gmail.com>
2016-04-13 16:10:36 +0100

    Add styling and login redirect.

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var fs = require('fs.extra');
+var fs = require('fs-extra');
+
+var bodyParser = require('body-parser');
 
 var ip = 'localhost';
 var port = 3000;
-module.exports = {
-  "app": app,
-  "http": http,
-  "fs": fs,
-  "io": io,
-  "ip": ip,
-  "port": port
 
+
+var login = function(username){
+  fs.copy('data/sample.db', 'sessions/' + username + '.db' , function(err){
+    if (err) return console.error(err);
+  });
 }
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/style.css', function(req, res){
+  res.sendFile(__dirname + '/style.css');
+});
+
+app.get('/:user', function(req, res){
+  res.sendFile(__dirname + '/lesson.html');
+});
+app.use(bodyParser.urlencoded({extended: true}));
+app.post('/', function(req, res){
+  console.log("req:" + req.body);
+  if(req.body.username !== ""){
+    login(req.body.username);
+    res.redirect('/' + req.body.username);
+  }
+});
+
 app.get('/admin', function(req, res){
   res.sendFile(__dirname + '/admin.html');
 });
+
+
 
 io.on('connection', function(socket){
   // Listening for user input....
@@ -42,3 +60,14 @@ io.on('connection', function(socket){
 http.listen(port, ip, function(){
   console.log('listening on ' + ip + ':' + port);
 });
+
+module.exports = {
+  "app": app,
+  "http": http,
+  "login": login,
+  "fs": fs,
+  "io": io,
+  "ip": ip,
+  "port": port
+
+}
